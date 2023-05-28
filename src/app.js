06dc6cbc948d73ac72c442da331a97d2290087e4
danielroman11 <danielroman.05111@gmail.com>
2023-05-27 21:37:04 -0500

    Permitiendo Cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import employeesRoutes from "./routes/employees.routes.js";
+import cors from "cors"
 import indexRoutes from "./routes/index.routes.js";
 import "./config.js"
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.use("/api", employeesRoutes);
 app.use(indexRoutes);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import app from "./app.js"
 import { PORT } from "./config.js"
 
+
+
 const port = PORT || 3000
 app.listen(port, () => console.log('Listen on port: ' + port));
